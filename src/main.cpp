645677aaf2082f8de4799798e9fc1f9938200aa6
Alessandro Nesi <alexandronesi@gmail.com>
2026-08-15 17:30:39 +0200

    removed camera support - it is useless after all

--- src/main.cpp
+++ src/main.cpp
@@ -10,37 +10,12 @@
 #include <WiFi.h>
 #include <time.h>
 #include <WiFiCredentials.h>
-#include "esp_camera.h"
 #include "motor-utils.h"
 #include "time-utils.h"
-#include "stream-utils.h"
 
 portMUX_TYPE rssiMux = portMUX_INITIALIZER_UNLOCKED;
 
 Preferences prefs;
-
-// Camera settings
-// AI Thinker ESP32-CAM pin definition
-#define PWDN_GPIO_NUM 32
-#define RESET_GPIO_NUM -1
-#define XCLK_GPIO_NUM 0
-#define SIOD_GPIO_NUM 26
-#define SIOC_GPIO_NUM 27
-
-#define Y9_GPIO_NUM 35
-#define Y8_GPIO_NUM 34
-#define Y7_GPIO_NUM 39
-#define Y6_GPIO_NUM 36
-#define Y5_GPIO_NUM 21
-#define Y4_GPIO_NUM 19
-#define Y3_GPIO_NUM 18
-#define Y2_GPIO_NUM 5
-#define VSYNC_GPIO_NUM 25
-#define HREF_GPIO_NUM 23
-#define PCLK_GPIO_NUM 22
-
-// 4 for flash led or 33 for normal led
-#define LED_GPIO_NUM 4
 
 // Time settings
 
@@ -468,28 +443,6 @@ void initialiseWebServer()
         }
     });
 
-    server.on("/api/snapshot", HTTP_GET, [](AsyncWebServerRequest* request)
-    {
-        camera_fb_t* fb = esp_camera_fb_get(); // capture frame
-        if (!fb)
-        {
-            request->send(500, "text/plain", "Camera capture failed");
-            return;
-        }
-
-        AsyncWebServerResponse* response =
-            request->beginResponse_P(200, "image/jpeg", fb->buf, fb->len);
-        response->addHeader("Content-Disposition",
-                            "inline; filename=capture.jpg");
-        request->send(response);
-        esp_camera_fb_return(fb); // return buffer to driver
-    });
-
-    // MJPEG stream
-    server.on("/api/stream", HTTP_GET, [](AsyncWebServerRequest* request)
-    {
-        handleStream(request);
-    });
     server.begin();
 }
 
@@ -533,52 +486,6 @@ void setup()
     saveResetReason();
     Serial.println("Setup started!");
     pinMode(FLASH_PIN, OUTPUT);
-    // Init Camera
-    camera_config_t config;
-    config.ledc_channel = LEDC_CHANNEL_0;
-    config.ledc_timer = LEDC_TIMER_0;
-    config.pin_d0 = Y2_GPIO_NUM;
-    config.pin_d1 = Y3_GPIO_NUM;
-    config.pin_d2 = Y4_GPIO_NUM;
-    config.pin_d3 = Y5_GPIO_NUM;
-    config.pin_d4 = Y6_GPIO_NUM;
-    config.pin_d5 = Y7_GPIO_NUM;
-    config.pin_d6 = Y8_GPIO_NUM;
-    config.pin_d7 = Y9_GPIO_NUM;
-    config.pin_xclk = XCLK_GPIO_NUM;
-    config.pin_pclk = PCLK_GPIO_NUM;
-    config.pin_vsync = VSYNC_GPIO_NUM;
-    config.pin_href = HREF_GPIO_NUM;
-    config.pin_sccb_sda = SIOD_GPIO_NUM;
-    config.pin_sccb_scl = SIOC_GPIO_NUM;
-    config.pin_pwdn = PWDN_GPIO_NUM;
-    config.pin_reset = RESET_GPIO_NUM;
-    config.xclk_freq_hz = 20000000;
-    config.frame_size = FRAMESIZE_VGA;
-    config.pixel_format = PIXFORMAT_JPEG; // for streaming
-    // config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
-    config.fb_location = CAMERA_FB_IN_PSRAM;
-    config.jpeg_quality = 15;
-    config.fb_count = 4;
-    config.grab_mode = CAMERA_GRAB_LATEST;
-
-    // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
-    //                      for larger pre-allocated frame buffer.
-
-    esp_err_t err = esp_camera_init(&config);
-    for (size_t i = 0; i < 3; i++)
-    {
-        if (err != ESP_OK)
-        {
-            Serial.printf("Camera init failed with error 0x%x\n", err);
-            delay(500);
-            err = esp_camera_init(&config);
-        }
-        else
-        {
-            break;
-        }
-    }
 
     loadSettings();
 
